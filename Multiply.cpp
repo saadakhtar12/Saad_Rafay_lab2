a7dd91444748af134bd7944393747c5ded0cccab
saadakhtar12 <saadcarnot@gmail.com>
2018-09-13 23:05:50 +0500

    Multiply
    Co-Authored-By: rafay-tariq <rafay-tariq@users.noreply.github.com>

--- Multiply.cpp
+++ Multiply.cpp
@@ -1,8 +1,7 @@
 	void Class1::Transpose(int r, int c, int a[10][10], int b[10][10]) {
 
 		int i = 0, j = 0, transpose[10][10];
-
-		// finding transpose
+			// finding transpose
 		for (i = 0; i < r; ++i)
 		for (j = 0; j < c; ++j)
 		{
